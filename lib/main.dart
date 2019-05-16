44f6bde99875f7e9f874df50348340ac4af3fe8c
Jamie Sanson <jamie.sanson@trademe.co.nz>
2019-05-16 20:32:38 +1200

    Make app stateless

--- lib/main.dart
+++ lib/main.dart
@@ -11,35 +11,22 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: MyHomePage(title: 'Outrank'),
+      home: OutrankHomePage(title: 'Outrank'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class OutrankHomePage extends StatelessWidget {
+  OutrankHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: UserList(),
     );
