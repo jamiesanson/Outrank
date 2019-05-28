0989ba2da50980631695852f208bb62b268bef80
Jamie Sanson <jamie.sanson@trademe.co.nz>
2019-05-28 21:18:44 +1200

    Updates ranking screen to be less attractive

--- lib/screens/ranking/ranking_screen.dart
+++ lib/screens/ranking/ranking_screen.dart
@@ -2,29 +2,71 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class RankingScreen extends StatelessWidget {
+  // Builds the user list widget containing everyones rankings
   Widget userList() {
-    return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance
-          .collection('usergroups')
-          .document('lm3VNbV4vbfWdVwhY0w5')
-          .collection('users')
-          .snapshots(),
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
-        switch (snapshot.connectionState) {
-          case ConnectionState.waiting:
-            return new Text('Loading...');
-          default:
-            return new ListView(
-              children:
-                  snapshot.data.documents.map((DocumentSnapshot document) {
-                return new ListTile(
-                  title: new Text(document['name']),
-                );
-              }).toList(),
-            );
-        }
-      },
+    return Column(children: <Widget>[
+      Flexible(child: Text("This months top players"), flex: 1),
+      Flexible(
+          flex: 4,
+          child: Scaffold(
+              body: StreamBuilder<QuerySnapshot>(
+            stream: Firestore.instance
+                .collection('users')
+                .snapshots(),
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasError)
+                return Text('Error: ${snapshot.error}');
+              switch (snapshot.connectionState) {
+                case ConnectionState.waiting:
+                  return Text('Loading...');
+                default:
+                  return ListView(
+                    children: snapshot.data.documents
+                        .map((DocumentSnapshot document) {
+                      return ListTile(
+                        title: Text(document['name']),
+                        subtitle: Text("${document['wins']} wins this month"),
+                      );
+                    }).toList(),
+                  );
+              }
+            },
+          )))
+    ]);
+  }
+
+  // Builds the page header widget, with illustration, start game action and
+  // whether or not the table's in use
+  Widget pageHeader() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        Image.network(
+          "https://www.trademe.co.nz/trust-safety/media/285465/kevin-nest-trademe.png",
+          width: 200,
+          height: 200,
+        ),
+        Text("The table is free!"),
+        Padding(
+          padding: EdgeInsets.all(16),
+          child: Center(
+              child: Material(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0)),
+                  color: Color(0xFF3B71DC),
+                  clipBehavior: Clip.antiAlias, // Add This
+                  child: MaterialButton(
+                    minWidth: 300,
+                    height: 48,
+                    child: Text(
+                      "START PLAYING",
+                      style: TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                    onPressed: () {},
+                  ))),
+        )
+      ],
     );
   }
 
@@ -34,8 +76,8 @@ class RankingScreen extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          Flexible(child: Text("data"), flex: 1),
-          Flexible(child: userList(), flex: 1)
+          Flexible(child: pageHeader(), flex: 5),
+          Flexible(child: userList(), flex: 4)
         ],
       ),
     );
